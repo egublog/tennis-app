5027131ff2b5cc4eb8c8d3a8dd93c1a558fcf2aa
egublog <egukei0729@gmail.com>
2025-01-15 19:24:34 +0900

    refactor: about

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,29 +1,27 @@
-import { ThemedText } from "@/components/ThemedText";
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function AboutScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the About screen.</Text>
-      <Link href="/" style={styles.button}>
-        <ThemedText type="link">Go to home screen!</ThemedText>
-      </Link>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">About</ThemedText>
+      <ThemedView style={styles.content}>
+        <ThemedText>
+          このアプリはテニスのペアを作成するためのアプリです。
+        </ThemedText>
+      </ThemedView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 16,
   },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    marginTop: 20,
+  content: {
+    marginTop: 16,
   },
 });
